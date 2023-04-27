fea487812d1d83fdb1c167c7d70df547b7a42ce4
Dan <dan@denchcity.com>
2023-04-27 19:10:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Tutorial for React course.
---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+  {
+    id: 1,
+    name: "Leanne Grayson",
+    username: "Brett",
+    email: "Sincere@april.biz",
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "Shanna@mellisa.tv",
+  },
+  {
+    id: 3,
+    name: "Clemetine Bauch",
+    username: "Samantha",
+    email: "Nathan@yesenia.net",
+  },
+  {
+    id: 4,
+    name: "Patricia Lebsack",
+    username: "Karianne",
+    email: "Julianne.Conner@kory.org",
+  },
+  {
+    id: 5,
+    name: "Trouty McSquire",
+    username: "Troutington",
+    email: "Trouts@tidal.com",
+  },
+  {
+    id: 6,
+    name: "Tommy Chavwick",
+    username: "Tommy",
+    email: "Tommy@tidal.com",
+  },
+  {
+    id: 7,
+    name: "Kurtis Weissant",
+    username: "Kurt",
+    email: "K.Weiss@billy.biz",
+  },
+  {
+    id: 8,
+    name: "Glenna Glenson",
+    username: "Glenz45",
+    email: "Glenidge@bob.com",
+  },
+  {
+    id: 9,
+    name: "Clementine McMemontine",
+    username: "Clemz",
+    email: "Clemzmanz@duzroad.net",
+  },
+  {
+    id: 10,
+    name: "Dennis Menace",
+    username: "DenzMcMenz",
+    email: "Denmen@beano.co.uk",
+  },
+];
